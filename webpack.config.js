f9c16fc90a36c758a98fbcdc804ac8b2e8a62a8a
hungeraibin <hungeraibin@126.com>
2019-05-09 23:01:44 +0800

    配置tsconfig，webpack运行成功

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 module.exports = {
+  mode: 'development',
   entry: {
     index: './lib/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'AiBinUI',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
