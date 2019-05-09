750c718dc3f8c90817cc289273c312c3068673f8
王爱彬 <hungeraibin@126.com>
2019-05-09 17:24:02 +0800

    add awesome-typescript-loader

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: {
+    index: './lib/index.tsx'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+}
