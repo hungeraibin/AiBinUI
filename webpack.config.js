620477f1a034f9d3564c0b669db47b73c15eaa68
王爱彬 <hungeraibin@126.com>
2019-05-10 15:56:21 +0800

    externals 不打包 react 和 react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     index: './lib/index.tsx'
   },
@@ -26,5 +26,19 @@ module.exports = {
      new HtmlWebpackPlugin({
        template: 'index.html'
      })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    }    
+  }
 }
