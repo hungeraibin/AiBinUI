95dd55744b3a8e1202cd72b9853953bd0d0f10c0
王爱彬 <hungeraibin@126.com>
2019-05-12 16:06:04 +0800

    引入 svg ，根据 name 展示不同 icon

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './icons/wechat.svg';
+import './icons/alipay.svg';
 
 interface IconProps {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = () => {
+const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>icon</span>
+    <svg>
+      <use xlinkHref={`#${props.name}`}></use>
+    </svg>
   )
 }
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import Icon from './icon';
 
 ReactDOM.render(<div>
-  <Icon name="wechat"></Icon>
-</div>, document.body);
+  <Icon name="wechat"/>
+  <Icon name="alipay"/>
+</div>, document.getElementById('root'));
