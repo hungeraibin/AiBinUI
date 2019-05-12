8ee6694927aeffd417573d523130d04f7168d50d
王爱彬 <hungeraibin@126.com>
2019-05-12 17:16:22 +0800

    添加点击事件React.MouseEventHandler

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,11 +4,12 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="ab-icon">
+    <svg className="ab-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e.target);
+}
+
 ReactDOM.render(<div>
-  <Icon name="wechat"/>
-  <Icon name="alipay"/>
+  <Icon name="wechat" onClick={fn}/>
+  <Icon name="alipay" onClick={fn}/>
 </div>, document.getElementById('root'));
