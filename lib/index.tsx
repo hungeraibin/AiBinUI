2a5e3b40088894b813a991f55d5eb14df69fc621
王爱彬 <hungeraibin@126.com>
2019-05-13 18:20:58 +0800

    Index 中导出 Icon 组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-}
-
-ReactDOM.render(<div>
-  <Icon name="wechat" onClick={fn} className="father"/>
-  <Icon name="alipay" onClick={fn}/>
-</div>, document.getElementById('root'));
+export { default as Icon } from './icon/icon';
