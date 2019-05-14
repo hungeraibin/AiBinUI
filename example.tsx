6e41aaeb228bddb4f581b18e46eb2b7ad2684e47
王爱彬 <hungeraibin@126.com>
2019-05-14 20:33:24 +0800

    创建dialog组件并控制显示

--- example.tsx
+++ example.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ButtonExample from './lib/button/button.example';
 import IconExample from './lib/icon/icon.example';
+import DialogExample from './lib/dialog/dialog.example';
 
 ReactDOM.render(
   <Router>
@@ -22,11 +23,15 @@ ReactDOM.render(
             <li>
               <Link to="/Button">Button</Link>
             </li>
+            <li>
+              <Link to="/Dialog">Dialog</Link>
+            </li>
           </ul>
         </aside>
         <main>
           <Route path="/Icon" component={IconExample}></Route>
           <Route path="/Button" component={ButtonExample}></Route>
+          <Route path="/Dialog" component={DialogExample}></Route>
         </main>
       </div>
     </div>
---
+++ lib/dialog/dialog.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  visible: boolean;
+}
+
+const Dialog: React.FunctionComponent<Props> = (props) => {
+  return (
+    props.visible ?
+      <div>Dialog</div> :
+      null
+  );
+};
+
+export default Dialog;
