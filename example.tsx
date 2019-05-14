b10edfadee7ab6c7c31f2741f23d275dd2d6f5c0
王爱彬 <hungeraibin@126.com>
2019-05-14 20:19:21 +0800

    创建 buttion 文件夹

--- example.tsx
+++ example.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import ButtonExample from './lib/button.example';
+import ButtonExample from './lib/button/button.example';
 import IconExample from './lib/icon/icon.example';
 
 ReactDOM.render(
