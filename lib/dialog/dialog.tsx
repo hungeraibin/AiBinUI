8016ba99e435467f8150b2f55fe812a8092f73f9
王爱彬 <hungeraibin@126.com>
2019-05-14 20:55:05 +0800

    引入 SCSS 添加基本样式

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './dialog.scss';
+import Icon from '../icon/icon';
 
 interface Props {
   visible: boolean;
@@ -7,7 +9,25 @@ interface Props {
 const Dialog: React.FunctionComponent<Props> = (props) => {
   return (
     props.visible ?
-      <div>{props.children}</div> :
+      <Fragment>
+        <div className="ab-dialog-mask"></div>
+        <div className="ab-dialog">
+          <div className="ab-dialog-close">
+            <Icon name="close"></Icon>
+          </div>
+          <header className="ab-dialog-header">
+            提示
+          </header>
+          <main className="ab-dialog-main">
+            {props.children}
+          </main>
+          <footer className="ab-dialog-footer">
+            <button>ok</button>
+            <button>cancel</button>
+          </footer>
+        </div>
+      </Fragment>
+      :
       null
   );
 };
