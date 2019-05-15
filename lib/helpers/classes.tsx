bd1130bb8a49f8c219cc6de71020140d4d324c29
王爱彬 <hungeraibin@126.com>
2019-05-15 21:26:10 +0800

    使用函数式重构 scopedClassMarker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -12,27 +12,16 @@ interface ClassToggles {
   [K: string]: boolean
 }
 
-function scopedClassMarker(prefix: string) {
-  return function(name: string | ClassToggles, options?: Options) {
-    let name2;
-    let result;
-
-    if (typeof name === 'string' || name === undefined) {
-      name2 = name;
-      result = [prefix, name2].filter(Boolean).join('-');
-    } else {
-      name2 = Object.entries(name).filter(kv=>kv[1]).map(kv => kv[0])
-      result = name2.map(n => {
-        return [prefix, n].filter(Boolean).join('-');
-      }).join(' ');
-    }
-
-    if (options && options.extra) {
-      return [result, options && options.extra].filter(Boolean).join(' ');
-    } else {
-      return result;
-    }
-  };
-};
+const scopedClassMarker = (prefix: string) => 
+  (name: string | ClassToggles, options?: Options) => 
+    Object
+      .entries(name instanceof Object ? name : {[name]: name})
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
 
 export {scopedClassMarker};
