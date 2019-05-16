68ea83bd3c5374ba7ab317a48be782ad956a6215
王爱彬 <hungeraibin@126.com>
2019-05-16 10:01:15 +0800

    美化 Layout Example

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -5,39 +5,40 @@ import Header from './header';
 import Content from './content';
 import Footer from './footer';
 import Aside from './aside';
+import './layout.example.scss';
 
 export default function() {
   return (
     <div>
       <div>
         <h1>exmaple1</h1>
-        <Layout className="hello" style={{height: 300}}>
-          <Header>Header</Header>
-          <Content>Content</Content> 
-          <Footer>Footer</Footer>
+        <Layout style={{height: 300, width: 300}}>
+          <Header className="x">Header</Header>
+          <Content className="y">Content</Content> 
+          <Footer className="x">Footer</Footer>
         </Layout>
       </div>
 
       <div>
         <h1>exmaple2</h1>
-        <Layout className="hello" style={{height: 300}}>
-          <Header>Header</Header>
+        <Layout style={{height: 300, width: 300}}>
+          <Header className="x">Header</Header>
           <Layout>
-            <Aside>Aside</Aside>
-            <Content>Content</Content> 
+            <Aside className="z">Aside</Aside>
+            <Content className="y">Content</Content> 
           </Layout>
-          <Footer>Footer</Footer>
+          <Footer className="x">Footer</Footer>
         </Layout>
       </div>
 
       <div>
         <h1>exmaple3</h1>
-        <Layout className="hello" style={{height: 300}}>
-          <Aside>Aside</Aside>
+        <Layout style={{height: 300, width: 300}}>
+          <Aside className="z">Aside</Aside>
           <Layout>
-            <Header>Header</Header>
-            <Content>Content</Content> 
-            <Footer>Footer</Footer>
+            <Header className="x">Header</Header>
+            <Content className="y">Content</Content> 
+            <Footer className="x">Footer</Footer>
           </Layout>
         </Layout>
       </div>
