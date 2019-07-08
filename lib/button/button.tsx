8a8a1156e854021b5eae484a9092b00a476bad0d
aibin <hungeraibin@126.com>
2019-07-08 11:26:18 +0800

    完成 button 组件

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
+import classes from '../helpers/classes';
+import './button.scss';
 
-function Button() {
-  return (
-    <div>按钮</div>
-  );
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  level?: 'important' | 'danger' | 'normal'
 }
+
+const Button: React.FunctionComponent<Props> = (props) => {
+  const { className, children, level, ...rest } = props;
+  return (
+    <button 
+      className={ classes('ab-button', `ab-button-${level}`, className) }
+      { ...rest }
+    >
+      { children }
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  level: 'normal'
+};
 
 export default Button;
